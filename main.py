cda9de9849566126d32470acf45323a7d8655d10
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2026-06-04 19:36:12 -0500

    Init project

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+GENI LIB requires Python 2x.
+
+Avoid using Python 3x features.
+
+But because static checks only work with python 3x.
+Make sure your config works with both python 2 and 3.
+
+"""
+
+
+def main():
+    print("Hello from online-boutique!")
+
+
+if __name__ == "__main__":
+    main()
